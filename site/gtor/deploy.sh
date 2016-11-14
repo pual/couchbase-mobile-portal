e92a7b73b0b8accafba5be8bfa1628f85e641358
James Nocentini <james.nocentini@gmail.com>
2016-11-14 09:18:39 -0800

    Update site map

--- site/gtor/deploy.sh
+++ site/gtor/deploy.sh
@@ -36,15 +36,13 @@ if [[ ${1} = "build" ]]; then
 	
 	cp -rf ${OPENID} "${SOURCE_FOLDER}"
 	cp -rf ${INSTALLATION} "${SOURCE_FOLDER}"
-	cp -rf ${DEVELOP} "${SOURCE_FOLDER}/develop"
+	cp -rf ${DEVELOP} "${SOURCE_FOLDER}"
 	cp -rf ${TRAINING} "${SOURCE_FOLDER}"
 	
 	rm -rf ../../tmp/ready/training/using-the-database/img/
 	rm -rf ../../tmp/ready/training/adding-synchronization/img/
 	rm -rf ../../tmp/ready/training/adding-security/img/
-	
-	cp -rf ${TRAINING} "${SOURCE_FOLDER}/develop"
-	
+		
 	echo "Building Swagger..."
 	cd ./../../swagger
 	gulp build
